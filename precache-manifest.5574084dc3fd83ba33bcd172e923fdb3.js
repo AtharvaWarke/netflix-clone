485057d57ee58f00a5e00539ee78741d06f4a423
AtharvaWarke <avwarke@asu.edu>
2022-06-13 21:54:40 +0530

    Updates

---
+++ precache-manifest.5574084dc3fd83ba33bcd172e923fdb3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbaa316e4d290069acdb",
+    "url": "/netflix-clone/static/css/main.727e7dfa.chunk.css"
+  },
+  {
+    "revision": "dbaa316e4d290069acdb",
+    "url": "/netflix-clone/static/js/main.dbaa316e.chunk.js"
+  },
+  {
+    "revision": "767128f02ae35c306b00",
+    "url": "/netflix-clone/static/js/reactPlayerYouTube.767128f0.chunk.js"
+  },
+  {
+    "revision": "2ca8dbffe6b7f02696d0",
+    "url": "/netflix-clone/static/js/reactPlayerSoundCloud.2ca8dbff.chunk.js"
+  },
+  {
+    "revision": "0cb0ca11266b42d762ac",
+    "url": "/netflix-clone/static/js/reactPlayerVimeo.0cb0ca11.chunk.js"
+  },
+  {
+    "revision": "3aaf42add7bdc4a3be48",
+    "url": "/netflix-clone/static/js/reactPlayerFacebook.3aaf42ad.chunk.js"
+  },
+  {
+    "revision": "2987c96dfddca453e7f0",
+    "url": "/netflix-clone/static/js/reactPlayerStreamable.2987c96d.chunk.js"
+  },
+  {
+    "revision": "e91abc48cd86ce13dd17",
+    "url": "/netflix-clone/static/js/reactPlayerWistia.e91abc48.chunk.js"
+  },
+  {
+    "revision": "2cfe25c2fa28573167f4",
+    "url": "/netflix-clone/static/js/reactPlayerTwitch.2cfe25c2.chunk.js"
+  },
+  {
+    "revision": "ec3440b9164c18da65cb",
+    "url": "/netflix-clone/static/js/reactPlayerDailyMotion.ec3440b9.chunk.js"
+  },
+  {
+    "revision": "e65085e26a4d7f7718d9",
+    "url": "/netflix-clone/static/js/reactPlayerMixcloud.e65085e2.chunk.js"
+  },
+  {
+    "revision": "66117e9cbfb81d04b3f6",
+    "url": "/netflix-clone/static/js/reactPlayerVidyard.66117e9c.chunk.js"
+  },
+  {
+    "revision": "1d750dedb8ae5b252c86",
+    "url": "/netflix-clone/static/js/reactPlayerKaltura.1d750ded.chunk.js"
+  },
+  {
+    "revision": "1b929958ab389a696a12",
+    "url": "/netflix-clone/static/js/reactPlayerFilePlayer.1b929958.chunk.js"
+  },
+  {
+    "revision": "2d1b0f5a4aff81842949",
+    "url": "/netflix-clone/static/js/reactPlayerPreview.2d1b0f5a.chunk.js"
+  },
+  {
+    "revision": "b561ccb24c75b05d1a3f",
+    "url": "/netflix-clone/static/js/14.b561ccb2.chunk.js"
+  },
+  {
+    "revision": "51ef102066458ac73f3d",
+    "url": "/netflix-clone/static/js/15.51ef1020.chunk.js"
+  },
+  {
+    "revision": "a05f80aa3f322a32e7b1",
+    "url": "/netflix-clone/static/js/runtime~main.a05f80aa.js"
+  },
+  {
+    "revision": "42614f6ca06471a591801a3c2c58d905",
+    "url": "/netflix-clone/index.html"
+  }
+];
